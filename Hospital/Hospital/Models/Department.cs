efe03598a3151874358c8bfc6455983578250314
arsenmykich <arsenmykich67@gmail.com>
2024-03-22 14:53:38 +0200

    Added models, added DBContext, checked migration, added some packages

---
+++ Hospital/Hospital/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿namespace Hospital.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.ComponentModel.DataAnnotations;
+    public class Department
+    {
+
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public string Speciality { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public ICollection<Doctor> Doctors { get; set; }
+    }
+}
