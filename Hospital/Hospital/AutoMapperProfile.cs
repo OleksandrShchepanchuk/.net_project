a48d26942537a57687a0f16a4fb76948e2916a6d
arsenmykich <arsenmykich67@gmail.com>
2024-04-21 12:12:22 +0300

    FixedAutoMapper

--- Hospital/Hospital/AutoMapperProfile.cs
+++ Hospital/Hospital/AutoMapperProfile.cs
@@ -10,7 +10,23 @@ namespace Hospital
             CreateMap<Article, ArticleDTO>();
             CreateMap<ArticleDTO, Article>();
 
+            CreateMap<Department, DepartmentDTO>();
+            CreateMap<DepartmentDTO, Department>();
 
+            CreateMap<Doctor, DoctorDTO>();
+            CreateMap<DoctorDTO, Doctor>();
+
+            CreateMap<Review, ReviewDTO>();
+            CreateMap<ReviewDTO, Review>();
+
+            CreateMap<Role, RoleDTO>();
+            CreateMap<RoleDTO, Role>();
+
+            CreateMap<User, UserDTO>();
+            CreateMap<UserDTO, User>();
+
+            CreateMap<Wish, WishDTO>();
+            CreateMap<WishDTO, Wish>();
         }
     }
 }
