0e299bc54b7c846a7a2012fb358b83e60466eb2d
arsenmykich <arsenmykich67@gmail.com>
2024-05-10 13:33:04 +0300

    Added Video Model, DTO and Controller, modified mapper, updated DbContext

---
+++ Hospital/Hospital/Controllers/VideosController.cs
@@ -0,0 +1,113 @@
+﻿using AutoMapper;
+using Hospital.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Hospital.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideosController : ControllerBase
+    {
+        private readonly HospitalContext _context;
+        private readonly IMapper _mapper;
+
+        public VideosController(HospitalContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET: api/Videos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<VideoDTO>>> GetVideos()
+        {
+            var videos = await _context.Videos.ToListAsync();
+            return _mapper.Map<List<VideoDTO>>(videos);
+        }
+
+        // GET: api/Videos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<VideoDTO>> GetVideo(int id)
+        {
+            var video = await _context.Videos.FindAsync(id);
+
+            if (video == null)
+            {
+                return NotFound();
+            }
+
+            return _mapper.Map<VideoDTO>(video);
+        }
+
+        // PUT: api/Videos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVideo(int id, VideoDTO videoDTO)
+        {
+            if (id != videoDTO.VideoId)
+            {
+                return BadRequest();
+            }
+
+            var video = await _context.Videos.FindAsync(id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(videoDTO, video);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VideoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Videos
+        [HttpPost]
+        public async Task<ActionResult<VideoDTO>> PostVideo(VideoDTO videoDTO)
+        {
+            var video = _mapper.Map<Video>(videoDTO);
+            _context.Videos.Add(video);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVideo", new { id = video.VideoId }, videoDTO);
+        }
+
+        // DELETE: api/Videos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVideo(int id)
+        {
+            var video = await _context.Videos.FindAsync(id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+
+            _context.Videos.Remove(video);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VideoExists(int id)
+        {
+            return _context.Videos.Any(e => e.VideoId == id);
+        }
+    }
+}
---
+++ Hospital/Hospital/Models/Video.cs
@@ -0,0 +1,16 @@
+﻿namespace Hospital.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.ComponentModel.DataAnnotations;
+    public class Video
+    {
+        public int VideoId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public string Image { get; set; }
+    }
+}
